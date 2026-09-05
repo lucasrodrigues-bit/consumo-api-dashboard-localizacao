254fb0c4078d9786af9f0543542e21a2c9593417
lucasrodrigues-bit <lucasdeiror@gmail.com>
2026-09-04 23:30:42 -0300

    adicionando tratamento de exeções na main para a chamada do método buscarLocal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,27 @@
+import service.GeoLocationService;
+
+import java.util.Scanner;
 
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        GeoLocationService geoService = new GeoLocationService();
+
+        //Tratamento de exeções
+        try {
+
+            System.out.println("Digite o ip ou cep desejado:");
+            String entrada = input.nextLine();
+
+            //chama a função direto classe GeoLocationService()
+            System.out.println(geoService.buscarLocal(entrada));
+
+        } catch (Exception e){
+            System.out.println("Deu erro:"+ e.getMessage());
+
+        }
+
 
 
     }
