dd8e9fefcb7d9a61fd11bdce222cf2f31b46aa31
lucasrodrigues-bit <lucasdeiror@gmail.com>
2026-09-05 01:47:36 -0300

    Alterando retorno dos Métodos e Classe para LocalInfo

--- src/main/java/service/GeoLocationService.java
+++ src/main/java/service/GeoLocationService.java
@@ -18,19 +18,20 @@ import com.google.gson.Gson;
 
 public class GeoLocationService {
 
-    public String buscarLocal(String entrada) throws IOException, InterruptedException {
+    //Método que indentifica se a busca é por Ip ou Cep
+    public LocalInfo buscarLocal(String entrada) throws IOException, InterruptedException {
 
         //Decidindo se a busca é por ip ou cep
         if (entrada != null && !entrada.isEmpty()) {
 
             if (entrada.contains(".")) {
-                String resultado_BuscaIp = buscarPorIp(entrada);
+                LocalInfo resultado_BuscaIp = buscarPorIp(entrada);
                 return resultado_BuscaIp;
 
             }
 
             else {
-                String resultado_BuscaCep = buscarPorCep(entrada);
+                LocalInfo resultado_BuscaCep = buscarPorCep(entrada);
                 return resultado_BuscaCep;
 
             }
@@ -42,7 +43,7 @@ public class GeoLocationService {
 
 //------------------------------------------------------------//---------------------------------------------------------
     //Faz a busca por Ip para caso seja fora do aís(Brasil)
-   private String buscarPorIp(String ip) throws IOException, InterruptedException {
+   private LocalInfo buscarPorIp(String ip) throws IOException, InterruptedException {
 
        //Criando HttpCliente "Carteiro" sabe como entregar dados e trazer respostas de volta
        HttpClient client = HttpClient.newHttpClient();
@@ -70,10 +71,8 @@ public class GeoLocationService {
        //Recebendo resposta do envelope(Response)
        HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
 
-
-
        //Lendo resposta do envelope
-       int status = response.statusCode();
+        int status = response.statusCode();
         String corpo = response.body();
 
        //Converte resposta do envelope(Json) em Objeto para a classe LocalInfo
@@ -87,7 +86,8 @@ public class GeoLocationService {
 
 //-------------------------------------------------------//-------------------------------------------------------------
    //Busca por Cep caso seja dentro do país(Brasil)
-   private String buscarPorCep(String cep)  throws IOException, InterruptedException {
+   private LocalInfo buscarPorCep(String cep)  throws IOException, InterruptedException {
+
        //Criando HttpCliente "Carteiro" sabe como entregar dados e trazer respostas de volta
        HttpClient client = HttpClient.newHttpClient();
 
@@ -100,7 +100,6 @@ public class GeoLocationService {
        //Lendo resposta do envelope
        int status = response.statusCode();
        String corpo = response.body();
-       return corpo;
 
        //Converte resposta do envelope(Json) em Objeto para a classe LocalInfo
         Gson gson = new Gson();
