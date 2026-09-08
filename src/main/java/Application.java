94d9d9e7ea5ed5fad13fad92ee77c9b5b9534683
lucasrodrigues-bit <lucasdeiror@gmail.com>
2026-09-08 05:02:38 -0300

    Pequenas correções de nomenclatura e

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,7 +3,7 @@ import service.WeatherService;
 
 import java.util.Scanner;
 
-public class Main {
+public class Application {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
