b00567db48737ccda62add5c25410c50c5814fd8
lucasrodrigues-bit <lucasdeiror@gmail.com>
2026-09-08 04:45:29 -0300

    Adicionando segundo serviço

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import service.GeoLocationService;
+import service.WeatherService;
 
 import java.util.Scanner;
 
@@ -12,15 +13,31 @@ public class Main {
         try {
 
             System.out.println("Digite o ip ou cep(Brasil) desejado:");
-            String entrada = input.nextLine();
+            String localidade = input.nextLine();
 
             //chama a função direto classe GeoLocationService()
-            System.out.println(geoService.buscarLocal(entrada));
+            System.out.println(geoService.buscarLocal(localidade));
 
         } catch (Exception e){
             System.out.println("Deu erro:"+ e.getMessage());
 
         }
+
+        Scanner entrada = new Scanner(System.in);
+        WeatherService weatherService = new WeatherService();
+        try {
+
+            System.out.println("Digite o nome da cidade:");
+            String localidade = entrada.nextLine();
+
+            System.out.println(weatherService.DadosMeteorologicos(localidade));
+
+        } catch (Exception e){
+            System.out.println("Deu erro:"+ e.getMessage());
+
+        }
+
+
 
 
 
