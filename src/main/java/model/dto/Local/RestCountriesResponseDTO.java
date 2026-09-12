4d58cdd1f79d12e2e0b2fea7d4694bd50b5a23de
lucasrodrigues-bit <lucasdeiror@gmail.com>
2026-09-11 23:23:37 -0300

    Alteração do DTO para conseguir guardar a nov aestrutura que a API retorna

--- src/main/java/model/dto/Local/RestCountriesResponseDTO.java
+++ src/main/java/model/dto/Local/RestCountriesResponseDTO.java
@@ -1,24 +1,59 @@
 package model.dto.Local;
 
+import java.util.List;
 import java.util.Map;
 
 public class RestCountriesResponseDTO {
-    private NameInfo name;
-    private Map<String, CurrencyDetail> currencies;
 
-    public NameInfo getName() { return name; }
-    public Map<String, CurrencyDetail> getCurrencies() { return currencies; }
+    private DataInfo data;
 
+    public DataInfo getData() {
+        return data;
+    }
+
+    public static class DataInfo {
+
+        private List<CountryInfo> objects;
+
+        public List<CountryInfo> getObjects() {
+            return objects;
+        }
+    }
+
+    public static class CountryInfo {
+
+        private NameInfo names;
+        private Map<String, CurrencyDetail> currencies;
+
+        public NameInfo getNames() {
+            return names;
+        }
+
+        public Map<String, CurrencyDetail> getCurrencies() {
+            return currencies;
+        }
+    }
 
     public static class NameInfo {
+
         private String common;
-        public String getCommon() { return common; }
+
+        public String getCommon() {
+            return common;
+        }
     }
 
     public static class CurrencyDetail {
+
         private String name;
         private String symbol;
-        public String getName() { return name; }
-        public String getSymbol() { return symbol; }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getSymbol() {
+            return symbol;
+        }
     }
 }
